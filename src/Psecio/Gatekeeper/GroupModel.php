cb54cf2d23ea2b5a5d553172d905dcf0e757077f
Chris Cornutt <enygma@phpdeveloper.org>
2014-12-11 21:32:29 -0600

    adding the Group model

---
+++ src/Psecio/Gatekeeper/GroupModel.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace Psecio\Gatekeeper;
+
+class GroupModel extends \Psecio\Gatekeeper\Model\Mysql
+{
+    /**
+     * Database table name
+     * @var string
+     */
+    protected $tableName = 'groups';
+
+    /**
+     * Model properties
+     * @var array
+     */
+    protected $properties = array(
+        'name' => array(
+            'description' => 'Group Name',
+            'column' => 'name',
+            'type' => 'varchar'
+        ),
+        'id' => array(
+            'description' => 'Group ID',
+            'column' => 'id',
+            'type' => 'integer'
+        ),
+        'created' => array(
+            'description' => 'Date Created',
+            'column' => 'created',
+            'type' => 'datetime'
+        ),
+        'updated' => array(
+            'description' => 'Date Updated',
+            'column' => 'updated',
+            'type' => 'datetime'
+        )
+    );
+
+    /**
+     * Add a user to the group
+     *
+     * @param integer|UserModel $user Either a user ID or a UserModel instance
+     */
+    public function addUser($user)
+    {
+        if ($this->id === null) {
+            return false;
+        }
+        if ($user instanceof UserModel) {
+            $user = $user->id;
+        }
+        $data = array(
+            'group_id' => $this->id,
+            'user_id' => $user
+        );
+        $groupUser = new UserGroupModel($this->getDb(), $data);
+        $groupUser->save();
+    }
+
+    /**
+     * Check if the user is in the current group
+     *
+     * @param integer $userId User ID
+     * @return boolean Found/not found in group
+     */
+    public function inGroup($userId)
+    {
+        $userGroup = new UserGroupModel($this->getDb());
+        $result = $userGroup->find(array(
+            'group_id' => $this->id,
+            'user_id' => $userId
+        ));
+        return ($userGroup->id !== null) ? true : false;
+    }
+}
