54c6bf09dbf11ff91774dcebff6a2fa4c8e42f70
Chris Cornutt <enygma@phpdeveloper.org>
2014-12-11 21:32:53 -0600

    adding model for user group

---
+++ src/Psecio/Gatekeeper/UserGroupModel.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Psecio\Gatekeeper;
+
+class UserGroupModel extends \Psecio\Gatekeeper\Model\Mysql
+{
+    /**
+     * Database table name
+     * @var string
+     */
+    protected $tableName = 'user_group';
+
+    /**
+     * Model properties
+     * @var array
+     */
+    protected $properties = array(
+        'group_id' => array(
+            'description' => 'Group Id',
+            'column' => 'group_id',
+            'type' => 'integer'
+        ),
+        'user_id' => array(
+            'description' => 'User ID',
+            'column' => 'user_id',
+            'type' => 'integer'
+        ),
+        'id' => array(
+            'description' => 'ID',
+            'column' => 'id',
+            'type' => 'integer'
+        ),
+        'created' => array(
+            'description' => 'Date Created',
+            'column' => 'created',
+            'type' => 'datetime'
+        ),
+        'updated' => array(
+            'description' => 'Date Updated',
+            'column' => 'updated',
+            'type' => 'datetime'
+        )
+    );
+}
