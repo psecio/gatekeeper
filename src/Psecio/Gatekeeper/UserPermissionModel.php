1a43272256577aebfab3fcbb55238c05211821bb
Chris Cornutt <enygma@phpdeveloper.org>
2014-12-13 16:33:29 -0600

    adding model for user -> permission relationship

---
+++ src/Psecio/Gatekeeper/UserPermissionModel.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Psecio\Gatekeeper;
+
+class UserPermissionModel extends \Psecio\Gatekeeper\Model\Mysql
+{
+    /**
+     * Database table name
+     * @var string
+     */
+    protected $tableName = 'user_permission';
+
+    /**
+     * Model properties
+     * @var array
+     */
+    protected $properties = array(
+        'permission_id' => array(
+            'description' => 'Permission Id',
+            'column' => 'permission_id',
+            'type' => 'integer'
+        ),
+        'user_id' => array(
+            'description' => 'User ID',
+            'column' => 'user_id',
+            'type' => 'integer'
+        ),
+        'id' => array(
+            'description' => 'ID',
+            'column' => 'id',
+            'type' => 'integer'
+        ),
+        'created' => array(
+            'description' => 'Date Created',
+            'column' => 'created',
+            'type' => 'datetime'
+        ),
+        'updated' => array(
+            'description' => 'Date Updated',
+            'column' => 'updated',
+            'type' => 'datetime'
+        )
+    );
+}
