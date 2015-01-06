82048f4fb80f78c69dc5a7e4f88d19bd2b7ded89
Chris Cornutt <chris.cornutt@hp.com>
2015-01-06 14:44:38 -0600

    adding more Group model tests, updating model to fix a bug

--- tests/Psecio/Gatekeeper/GroupModelTest.php
+++ tests/Psecio/Gatekeeper/GroupModelTest.php
@@ -49,4 +49,59 @@ class GroupModelTest extends \PHPUnit_Framework_TestCase
 
         $this->assertFalse($group->addUser(1));
     }
+
+    /**
+     * Test adding a permission by ID
+     */
+    public function testAddPermissionByIdValid()
+    {
+        $ds = $this->buildMock(true, 'save');
+        $group = new GroupModel($ds, array('id' => 1234));
+
+        $this->assertTrue($group->addPermission(1));
+    }
+
+    /**
+     * Test adding a permission by a model instance
+     */
+    public function testAddPermissionByModelValid()
+    {
+        $ds = $this->buildMock(true, 'save');
+        $group = new GroupModel($ds, array('id' => 1234));
+        $perm = new PermissionModel($ds, array('id' => 1234));
+
+        $this->assertTrue($group->addPermission($perm));
+    }
+
+    /**
+     * Test adding a permission by ID on an invalid group
+     */
+    public function testAddPermissionByIdInvalid()
+    {
+        $ds = $this->buildMock(true, 'save');
+        $group = new GroupModel($ds);
+
+        $this->assertFalse($group->addPermission(1));
+    }
+
+    /**
+     * Test that a user is in a group
+     */
+    public function testUserInGroup()
+    {
+        $data = array(
+            array('name' => 'group1', 'id' => 1234)
+        );
+        $ds = $this->getMockBuilder('\Psecio\Gatekeeper\DataSource\Mysql')
+            ->disableOriginalConstructor()
+            ->setMethods(array('fetch'))
+            ->getMock();
+
+        $ds->method('fetch')
+            ->willReturn($data);
+
+        $group = new GroupModel($ds);
+
+        $this->assertTrue($group->inGroup(1));
+    }
 }
