aa36a1b94e5c4f3f1644285bc24f946a8642b07a
Chris Cornutt <chris.cornutt@hp.com>
2015-01-15 11:02:57 -0600

    adding test to find child groups

--- tests/Psecio/Gatekeeper/GroupCollectionTest.php
+++ tests/Psecio/Gatekeeper/GroupCollectionTest.php
@@ -21,4 +21,25 @@ class GroupCollectionTest extends \Psecio\Gatekeeper\Base
         $groups->findByUserId($userId);
         $this->assertCount(2, $groups);
     }
+
+    /**
+     * Test the location and conversion of child groups into instances
+     */
+    public function testFindChildrenGroups()
+    {
+        $groupId = 1;
+        $return = array(
+            array('name' => 'group1', 'description' => 'Group #1'),
+            array('name' => 'group2', 'description' => 'Group #2')
+        );
+
+        $ds = $this->buildMock($return, 'fetch');
+        $groups = new GroupCollection($ds);
+
+        $groups->findChildrenByGroupId($groupId);
+        $this->assertCount(2, $groups);
+
+        $groups = $groups->toArray();
+        $this->assertTrue($groups[0] instanceof GroupModel);
+    }
 }
