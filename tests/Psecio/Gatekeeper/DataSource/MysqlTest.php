c7d38a8ec25eb0c6a132ca34a91abac090820c6f
Chris Cornutt <chris.cornutt@hp.com>
2015-01-15 11:02:40 -0600

    adding tests for get/set db, getting table name

--- tests/Psecio/Gatekeeper/DataSource/MysqlTest.php
+++ tests/Psecio/Gatekeeper/DataSource/MysqlTest.php
@@ -3,6 +3,7 @@
 namespace Psecio\Gatekeeper\DataSource;
 
 include_once __DIR__.'/../MockPdo.php';
+include_once __DIR__.'/../MockModel.php';
 
 class MysqlTest extends \Psecio\Gatekeeper\Base
 {
@@ -22,5 +23,39 @@ class MysqlTest extends \Psecio\Gatekeeper\Base
             ->getMock();
 
         $this->assertEquals($mysql->getDb(), $pdo);
+    }
+
+    /**
+     * Test the getter/setter of the DB instance
+     *     (just uses a basic object)
+     */
+    public function testGetSetDatabaseInstance()
+    {
+        $mysql = $this->getMockBuilder('\Psecio\Gatekeeper\DataSource\Mysql')
+            ->disableOriginalConstructor()
+            ->setMethods(array('buildPdo'))
+            ->getMock();
+
+        $db = (object)array('test' => 'foo');
+        $mysql->setDb($db);
+
+        $this->assertEquals($mysql->getDb(), $db);
+    }
+
+    /**
+     * Test getting the table name for the model instance
+     */
+    public function testGetTableName()
+    {
+        $config = array();
+        $pdo = $this->getMockBuilder('\Psecio\Gatekeeper\MockPdo')->getMock();
+
+        $ds = $this->getMockBuilder('\Psecio\Gatekeeper\DataSource\Mysql')
+            ->setConstructorArgs(array($config, $pdo))
+            ->setMethods(array('buildPdo'))
+            ->getMock();
+
+        $mysql = new \Psecio\Gatekeeper\MockModel($ds);
+        $this->assertEquals('test', $mysql->getTableName());
     }
 }
