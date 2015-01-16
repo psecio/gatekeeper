23305b2c6eb1cb04dead2c9b7ad8a6ecdf5864da
Chris Cornutt <chris.cornutt@hp.com>
2015-01-16 15:41:39 -0600

    adding UserPermissionCollection

---
+++ src/Psecio/Gatekeeper/UserPermissionCollection.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Psecio\Gatekeeper;
+
+class UserPermissionCollection extends \Psecio\Gatekeeper\Collection\Mysql
+{
+    /**
+     * Find the permissions for a given user ID
+     *
+     * @param integer $userId User ID
+     */
+    public function findByUserId($userId)
+    {
+        $data = array('userId' => $userId);
+        $sql = 'select p.* from permissions p, user_permission up'
+            .' where p.id = up.permission_id'
+            .' and up.user_id = :userId';
+
+        $results = $this->getDb()->fetch($sql, $data);
+
+        foreach ($results as $result) {
+            $perm = new PermissionModel($this->getDb(), $result);
+            $this->add($perm);
+        }
+    }
+
+    /**
+     * Create relational records linking the user and permission
+     *
+     * @param \Gatekeeper\UserModel $model Model instance
+     * @param array $data Data to use in create
+     */
+    public function create($model, array $data)
+    {
+        foreach ($data as $permission) {
+            // Determine if it's an integer (permissionId) or name
+            if (is_int($permission) === true) {
+                $where = 'id = :id';
+                $dbData = array('id' => $permission);
+            } else {
+                $where = 'name = :name';
+                $dbData = array('name' => $permission);
+            }
+
+            $sql = 'select id, name from permissions where '.$where;
+            $results = $this->getDb()->fetch($sql, array('name' => $permission));
+            if (!empty($results) && count($results) == 1) {
+                // exists, make the relation
+                $model = new UserPermissionModel(
+                    $this->getDb(),
+                    array('permissionId' => $results[0]['id'], 'userId' => $model->id)
+                );
+                $this->getDb()->save($model);
+            }
+        }
+    }
+}
