413aba0c02f70ef5eb02f9dcaed523405665ced7
Chris Cornutt <chris.cornutt@hp.com>
2015-01-16 15:41:01 -0600

    removing the findByUserId from Permission collection, moved to findByUserId

--- src/Psecio/Gatekeeper/PermissionCollection.php
+++ src/Psecio/Gatekeeper/PermissionCollection.php
@@ -5,26 +5,6 @@ namespace Psecio\Gatekeeper;
 class PermissionCollection extends \Psecio\Gatekeeper\Collection\Mysql
 {
 	/**
-	 * Find the permissions for a given user ID
-	 *
-	 * @param integer $userId User ID
-	 */
-	public function findByUserId($userId)
-	{
-		$data = array('userId' => $userId);
-		$sql = 'select p.* from permissions p, user_permission up'
-			.' where p.id = up.permission_id'
-			.' and up.user_id = :userId';
-
-		$results = $this->getDb()->fetch($sql, $data);
-
-        foreach ($results as $result) {
-            $perm = new PermissionModel($this->getDb(), $result);
-            $this->add($perm);
-        }
-	}
-
-	/**
 	 * Find the permissions for the given group ID
 	 *
 	 * @param integer $groupId Group ID
