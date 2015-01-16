6b92a87d63223cd25f92e3309f38952c40dca165
Chris Cornutt <chris.cornutt@hp.com>
2015-01-16 16:20:51 -0600

    updating PermssionCollection test, moving method to UserPermissionCollectionTest

--- tests/Psecio/Gatekeeper/UserPermissionCollectionTest.php
+++ tests/Psecio/Gatekeeper/UserPermissionCollectionTest.php
@@ -2,23 +2,23 @@
 
 namespace Psecio\Gatekeeper;
 
-class PermissionCollectionTest extends \Psecio\Gatekeeper\Base
+class UserPermissionCollectionTest extends \Psecio\Gatekeeper\Base
 {
     /**
-     * Test the location of permissions of a group by ID
+     * Test the location of permissions of a member by ID
      */
-    public function testFindPermissionsByGroupId()
+    public function testFindPermissionsByUserId()
     {
-        $groupId = 1;
+        $userId = 1;
         $return = array(
             array('name' => 'perm1', 'description' => 'Permission #1'),
             array('name' => 'perm2', 'description' => 'Permission #2')
         );
 
         $ds = $this->buildMock($return, 'fetch');
-        $permissions = new PermissionCollection($ds);
+        $permissions = new UserPermissionCollection($ds);
 
-        $permissions->findByGroupId($groupId);
+        $permissions->findByUserId($userId);
         $this->assertCount(2, $permissions);
     }
 }
