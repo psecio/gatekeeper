32fdf35be68ba48fd31f4952620d82707a41ab8e
Chris Cornutt <chris.cornutt@hp.com>
2015-01-16 16:32:38 -0600

    moving the findByUserId to the UserGroupCollection

--- src/Psecio/Gatekeeper/GroupCollection.php
+++ src/Psecio/Gatekeeper/GroupCollection.php
@@ -5,26 +5,6 @@ namespace Psecio\Gatekeeper;
 class GroupCollection extends \Psecio\Gatekeeper\Collection\Mysql
 {
     /**
-     * Find the groups that the given user belongs to
-     *
-     * @param integer $userId User ID
-     */
-    public function findByUserId($userId)
-    {
-        $data = array('userId' => $userId);
-        $sql = 'select g.* from groups g, user_group ug'
-            .' where ug.user_id = :userId'
-            .' and ug.group_id = g.id';
-
-        $results = $this->getDb()->fetch($sql, $data);
-
-        foreach ($results as $result) {
-            $group = new GroupModel($this->getDb(), $result);
-            $this->add($group);
-        }
-    }
-
-    /**
      * Find child groups by the parent group ID
      *
      * @param integer $groupId Group ID
