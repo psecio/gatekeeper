759cbfcacad70fdcb943e43c48c52677da5556e0
Chris Cornutt <enygma@phpdeveloper.org>
2015-01-25 23:26:24 +0100

    adding test for getting child permissions by perm id

--- tests/Psecio/Gatekeeper/PermissionCollectionTest.php
+++ tests/Psecio/Gatekeeper/PermissionCollectionTest.php
@@ -21,4 +21,22 @@ class PermissionCollectionTest extends \Psecio\Gatekeeper\Base
         $permissions->findByGroupId($groupId);
         $this->assertCount(2, $permissions);
     }
+
+    /**
+     * Test to ensure permissions are returned when searched by child id
+     */
+    public function testFindChidlrenByPermissionId()
+    {
+        $permId = 1;
+        $return = array(
+            array('name' => 'perm1', 'description' => 'Permission #1'),
+            array('name' => 'perm2', 'description' => 'Permission #2')
+        );
+
+        $ds = $this->buildMock($return, 'fetch');
+        $permissions = new PermissionCollection($ds);
+
+        $permissions->findChildrenByPermissionId($permId);
+        $this->assertCount(2, $permissions);
+    }
 }
