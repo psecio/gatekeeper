d50704482d2ee621882b72e5b58d3b5b08e87e9b
Chris Cornutt <enygma@phpdeveloper.org>
2015-02-18 11:24:41 -0500

    adding the collection for the security questions for a user

---
+++ src/Psecio/Gatekeeper/SecurityQuestionCollection.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Psecio\Gatekeeper;
+
+class SecurityQuestionCollection extends \Psecio\Gatekeeper\Collection\Mysql
+{
+	/**
+	 * Find the security questions for the given user ID
+	 *
+	 * @param integer $userId User ID
+	 */
+	public function findByUserId($userId)
+	{
+		$data = array('userId' => $userId);
+		$sql = 'select * from security_questions where user_id = :userId';
+
+		$results = $this->getDb()->fetch($sql, $data);
+
+        foreach ($results as $result) {
+            $question = new SecurityQuestionModel($this->getDb(), $result);
+            $this->add($question);
+        }
+	}
+}
