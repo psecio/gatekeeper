38366beb8b0b29031e91c2ac13a44f5176db05bd
Chris Cornutt <enygma@phpdeveloper.org>
2015-02-18 13:57:25 -0500

    adding tests for the "create" method in user permission collection

--- tests/Psecio/Gatekeeper/UserPermissionCollectionTest.php
+++ tests/Psecio/Gatekeeper/UserPermissionCollectionTest.php
@@ -21,4 +21,43 @@ class UserPermissionCollectionTest extends \Psecio\Gatekeeper\Base
         $permissions->findByUserId($userId);
         $this->assertCount(2, $permissions);
     }
+
+    /**
+     * Test the creation of a user permission relation by permission ID
+     */
+    public function testCreateUserPermissionById()
+    {
+        $return = array(
+            array('id' => 1, 'name' => 'perm5')
+        );
+
+        $ds = $this->buildMock($return, 'fetch');
+        $permissions = new UserPermissionCollection($ds);
+        $model = new UserPermissionModel($ds);
+
+        $data = array(1, 2, 3);
+        $permissions->create($model, $data);
+    }
+
+    /**
+     * Test the creation of a user permission relation by permission model instance
+     */
+    public function testCreateUserPermissionByModel()
+    {
+        $return = array(
+            array('id' => 1, 'name' => 'perm5'),
+            array('id' => 2, 'name' => 'perm6')
+        );
+
+        $ds = $this->buildMock($return, 'fetch');
+        $permissions = new UserPermissionCollection($ds);
+        $model = new UserPermissionModel($ds);
+
+        $data = array(
+            new PermissionModel($ds, array('id' => 1)),
+            new PermissionModel($ds, array('id' => 2)),
+            new PermissionModel($ds, array('id' => 3))
+        );
+        $permissions->create($model, $data);
+    }
 }
