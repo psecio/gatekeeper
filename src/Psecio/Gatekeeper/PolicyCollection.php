6b9f277ff312e9d690f1994c4c8671bf1d1b59b7
Chris Cornutt <enygma@phpdeveloper.org>
2015-05-29 22:04:22 -0500

    Adding a collection to fetch all policies

---
+++ src/Psecio/Gatekeeper/PolicyCollection.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Psecio\Gatekeeper;
+
+class PolicyCollection extends \Psecio\Gatekeeper\Collection\Mysql
+{
+	/**
+	 * Get the current list of policies
+	 *
+	 * @param integer $limit Limit the number of records
+	 */
+	public function getList($limit = null)
+	{
+		$sql = 'select * from policies';
+		if ($limit !== null) {
+			$sql.= ' limit '.$limit;
+		}
+		$results = $this->getDb()->fetch($sql);
+
+        foreach ($results as $result) {
+            $policy = new PolicyMoel($this->getDb(), $result);
+            $this->add($policy);
+        }
+	}
+}
