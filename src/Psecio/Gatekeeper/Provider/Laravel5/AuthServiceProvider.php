424be3085fe2779f0072ad323149b6826ea20273
Chris Cornutt <enygma@phpdeveloper.org>
2015-07-21 16:19:59 -0500

    adding a custom validator (gk_unique) to the provider for use with the Gatekeeper find* handling

--- src/Psecio/Gatekeeper/Provider/Laravel5/AuthServiceProvider.php
+++ src/Psecio/Gatekeeper/Provider/Laravel5/AuthServiceProvider.php
@@ -32,8 +32,26 @@ class AuthServiceProvider extends ServiceProvider
 	 */
 	public function boot(Router $router)
     {
+    	// Add Gatekeeper to the Auth provider list
 		Auth::extend('gatekeeper', function($app) {
 			return new UserProvider();
+		});
+
+		// Create a new "unique" (gkunique)  validator for unique user checking
+		Validator::extend('gk_unique', function($attribute, $value, $parameters) {
+			$type = (isset($parameters[0])) ? $parameters[0] : 'user';
+
+			// strip a training "s" if there is one
+			if (substr($type, -1) === 's') {
+				$type = substr($type, 0, strlen($type)-1);
+			}
+			$method = 'find'.ucwords($type).'By'.ucwords($attribute);
+			try {
+				$user = Gatekeeper::$method($value);
+				return ($user === false);
+			} catch (\Exception $e) {
+				return false;
+			}
 		});
 
         parent::boot($router);
